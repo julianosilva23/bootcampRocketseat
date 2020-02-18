755d52aa79d4e29ac016de4473f5418125b64e29
Juliano Silva <julianoluizsilva00@gmail.com>
2020-02-17 22:55:42 -0300

    validando entrada de dados

--- src/route.js
+++ src/route.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+routes.put('/users', UserController.update);
+// global middlewares // vale para rotas após
 
 export default routes;
