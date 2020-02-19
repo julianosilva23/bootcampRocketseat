3e156b0ebbeee8e6d5df4e63a3df99a821a5136b
Juliano Silva <julianoluizsilva00@gmail.com>
2020-02-18 21:13:50 -0300

    gerenciando upload de arquivos

--- src/route.js
+++ src/route.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
+import FileController from './app/controller/FileController';
 import authMiddleware from './app/middlewares/auth';
+import multerConfig from './config/multer';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -11,5 +15,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 // global middlewares // vale para rotas após
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
