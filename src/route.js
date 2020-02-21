8949c9f0da5147fc034c0e9ea8dc065e193cc37a
Juliano Silva <julianoluizsilva00@gmail.com>
2020-02-20 23:06:25 -0300

    Listando agendamentos do usuário com o avatar

--- src/route.js
+++ src/route.js
@@ -4,6 +4,7 @@ import UserController from './app/controller/UserController';
 import SessionController from './app/controller/SessionController';
 import FileController from './app/controller/FileController';
 import ProviderController from './app/controller/ProviderController';
+import AppointmentController from './app/controller/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
 
@@ -19,5 +20,8 @@ routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
