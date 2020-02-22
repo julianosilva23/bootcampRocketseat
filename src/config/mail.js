a899159f7479b7276f144804ab5f222b7ed6dc97
Juliano Silva <julianoluizsilva00@gmail.com>
2020-02-21 22:31:04 -0300

    Começando a configurar serviço de email

---
+++ src/config/mail.js
@@ -0,0 +1,10 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    secure: false,
+    port: 2525,
+    auth: {
+        user: 'bdf91b14b8c034',
+        pass: 'a6e8a3b3738a81',
+    },
+    default: { from: 'Equipe GoBarber <noreply@gobarber.com' },
+};
